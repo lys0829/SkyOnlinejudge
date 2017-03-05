ee9b7eaea3ff81900ed314247e7abccc9371b760
lys0829 <lys900829@gmail.com>
2017-03-05 18:39:30 +0800

    add ip option

--- config/config.example.php
+++ config/config.example.php
@@ -8,3 +8,4 @@ $_config['db']['dbpassword']    = '';
 $_config['db']['dbcharset']     = 'utf8';
 $_config['db']['tablepre']      = 'tojtest_';
 $_config['cookie']['namepre']   = 'tojtest';
+$_config['iplock'] = 0;//input 1 or 0(true or false)
